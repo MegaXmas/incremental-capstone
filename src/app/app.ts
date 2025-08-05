27f76886176e451a6590e9a9f23fa6dca8bc6eaf
MegaXmas <noahmendelsohn96@gmail.com>
2025-08-05 13:38:02 -0700

    testing proper boostrap code for sidebars

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { HeaderComponent } from './components/header/header';
+import { SidebarComponent } from "./components/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, HeaderComponent],
+  imports: [RouterOutlet, LoginComponent, HeaderComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
