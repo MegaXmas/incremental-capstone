4fa220921938f14495c867bd60d0b45ef022fc17
MegaXmas <noahmendelsohn96@gmail.com>
2025-08-06 16:34:21 -0700

    changing depriciated stuff

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { HeaderComponent } from './components/header/header';
+import { FlightFormComponent } from "./components/flight-form/flight-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, HeaderComponent],
+  imports: [RouterOutlet, LoginComponent, HeaderComponent, FlightFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
