c50f0065b35df28135f925e9241c103c78620c47
MegaXmas <noahmendelsohn96@gmail.com>
2025-08-07 19:33:27 -0700

    starting end user front end

---
+++ src/app/components/user-flight-booking/user-flight-booking.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-flight-booking',
+  imports: [],
+  templateUrl: './user-flight-booking.html',
+  styleUrl: './user-flight-booking.css'
+})
+export class UserFlightBooking {
+
+}
